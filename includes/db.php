20477b74890aa37e2b8b56c93c839bbbb442347f
balanghaiofficial <75129533+balanghaiofficial@users.noreply.github.com>
2020-11-30 12:36:32 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
 
-    $con = mysqli_connect("localhost", "root", "", "balanghai-cms");
+    $con = mysqli_connect("	f2fbe0zvg9j8p9ng.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "uls9h17813nvidc7", "c2d9e5l111tv0p4f", "fu2nww6lzyy25r50");
 
     if (!$con) {
         
