15a795ae6587170802268cc5c62319eb444d61a8
balanghaiofficial <75129533+balanghaiofficial@users.noreply.github.com>
2020-11-30 13:35:24 +0800

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -40,10 +40,12 @@ session_start();
             $result = mysqli_query($con, $query);
 
             while($row = mysqli_fetch_assoc($result)){
+                    
+                $cat_id = $row['cat_id']; 
 
                 $cat_title = $row['cat_title'];
 
-                echo " <li><a href='#'>{$cat_title}</a></li>";
+                echo " <li><a href='category.php?category={$cat_id}'>{$cat_title}</a></li>";
 
             }
 
